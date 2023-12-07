661fcf6e4e6fa3a9426e8bf15e1a1798dd9120a6
Jon <jnovins112@gmail.com>
2023-12-07 18:33:53 -0500

    added unit tests for tast2 compensation endpoints

--- src/test/java/com/mindex/challenge/service/impl/CompensationServiceImplTest.java
+++ src/test/java/com/mindex/challenge/service/impl/CompensationServiceImplTest.java
@@ -10,14 +10,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.Date;
-import java.util.UUID;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
