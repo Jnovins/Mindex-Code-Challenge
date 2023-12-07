dea8814ef832660cc1f0f52d4234af72a5b2cebb
Jon <jnovins112@gmail.com>
2023-12-07 18:33:09 -0500

    added unit tests for tast2 compensation endpoints

---
+++ src/test/java/com/mindex/challenge/service/impl/CompensationServiceImplTest.java
@@ -0,0 +1,96 @@
+package com.mindex.challenge.service.impl;
+
+import com.mindex.challenge.data.*;
+import com.mindex.challenge.service.CompensationService;
+import com.mindex.challenge.service.EmployeeService;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Date;
+import java.util.UUID;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class CompensationServiceImplTest {
+
+    private String employeeUrl;
+    private String compensationUrl;
+    private String compensationIdUrl;
+
+    @Autowired
+    private CompensationService compensationService;
+    private EmployeeService employeeService;
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Before
+    public void setup() {
+        employeeUrl = "http://localhost:" + port + "/employee";
+        compensationUrl = "http://localhost:" + port + "/compensation";
+        compensationIdUrl = "http://localhost:" + port + "/compensation/{id}";
+    }
+
+    @Test
+    public void testCreateRead() {
+
+        // Create our test employee and add it to the employees repository
+        Employee testEmployee = new Employee();
+        testEmployee.setFirstName("John");
+        testEmployee.setLastName("Doe");
+        testEmployee.setDepartment("Engineering");
+        testEmployee.setPosition("Developer");
+        testEmployee = restTemplate.postForEntity(employeeUrl, testEmployee, Employee.class).getBody();
+
+        // Test data
+        int testSalary = 10912;
+        Date testEffectiveDate = new Date();
+
+        // Build our test object
+        Compensation testCompensation = new Compensation();
+        testCompensation.setEmployee(testEmployee);
+        testCompensation.setSalary(testSalary);
+        testCompensation.setEffectiveDate(testEffectiveDate);
+
+        // Create checks
+        Compensation createdCompensation = restTemplate.postForEntity(compensationUrl, testCompensation, Compensation.class).getBody();
+
+        assertNotNull(createdCompensation);
+        assertNotNull(createdCompensation.getEmployee());
+        assertCompensationEquivalence(testCompensation, createdCompensation);
+
+        // Read checks
+        Compensation readCompensation = restTemplate.getForEntity(compensationIdUrl, Compensation.class, createdCompensation.getEmployee().getEmployeeId()).getBody();
+        assertNotNull(readCompensation);
+        assertCompensationEquivalence(createdCompensation, readCompensation);
+    }
+
+    private static void assertEmployeeEquivalence(Employee expected, Employee actual) {
+        assertEquals(expected.getFirstName(), actual.getFirstName());
+        assertEquals(expected.getLastName(), actual.getLastName());
+        assertEquals(expected.getDepartment(), actual.getDepartment());
+        assertEquals(expected.getPosition(), actual.getPosition());
+    }
+
+    private static void assertCompensationEquivalence(Compensation expected, Compensation actual) {
+        assertEmployeeEquivalence(expected.getEmployee(), actual.getEmployee());
+        assertEquals(expected.getSalary(), actual.getSalary());
+        assertEquals(expected.getEffectiveDate(), actual.getEffectiveDate());
+    }
+}
